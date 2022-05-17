283c8718bcc3da80d520eb034317d1a2198a43a4
GuillemXanxo <jscodebaker@gmail.com>
2022-05-17 17:03:52 +0200

    Functions for player movements

--- input/movements.go
+++ input/movements.go
@@ -6,4 +6,40 @@ type Sprite struct {
   Col int
 }
 
-var Player sprite
+var Player Sprite
+
+func makeMove (oldRow, oldCol int, direction string, maze []string) (newRow, newCol int) {
+  switch direction {
+  case "UP":
+    newRow = oldRow - 1
+    if newRow < 0 {
+      newRow = len(maze)-1
+    }
+  case "DOWN":
+    newRow = oldRow + 1
+    if newRow > len(maze) {
+      newRow = 0
+    }
+  case "LEFT":
+    newCol = oldCol - 1
+    if newCol < 0 {
+      newCol = len(maze[0])
+    }
+  case "RIGHT":
+    newCol = oldCol + 1
+    if newCol >= len(maze[0]) {
+      newCol = 0
+    }
+  }
+//if the movement hit a wall is cancelled
+  if maze[newRow][newCol] == '#' {
+    newRow = oldRow
+    newCol = oldCol
+  }
+
+  return
+}
+
+func MovePlayer(direction string, maze []string) {
+  Player.Row, Player.Col = makeMove(Player.Row, Player.Col, direction, maze)
+}
--- input/terminal.go
+++ input/terminal.go
@@ -39,6 +39,20 @@ func ReadFromTerminal() (string, error) {
 //if we are reading just one byte (cnt==1) and it is the ESC key (0x1b) then we return ESC
   if cnt == 1 && buffer[0] == 0x1b { 
     return "ESC", nil
+  } else if cnt >=3 {
+      if buffer[0] == 0x1b && buffer[1] == '[' {
+        switch buffer[2] {
+        case 'A':
+          return "UP", nil
+        case 'B':
+          return "DOWN", nil
+        case 'C':
+          return "RIGHT", nil
+        case 'D':
+          return "LEFT", nil
+        }
+      }
+    
   }
   return "", nil
 }
